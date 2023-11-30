9c04fae32eacb64d47b90325b7841691bc0e5c25
Prabeshika <prabeshika14@gmail.com>
2023-11-30 08:53:00 +0100

    Iteration 1 added

--- src/memory.js
+++ src/memory.js
@@ -1,10 +1,20 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
+    if (!this.card) {
+      return undefined;
+    }
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
     // ... write your code here
   }
 
